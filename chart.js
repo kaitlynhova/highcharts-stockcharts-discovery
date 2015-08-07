889f1b7d0a3672da2370ae8d824afd6aa9812f43
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-08-07 13:13:25 -0700

    starting over.
    *grabbing data from another outside source
    *first try at getting flags on selected points

--- chart.js
+++ chart.js
@@ -1,148 +1,55 @@
-$(function () {
-    var seriesOptions = [],
-        seriesCounter = 0,
-        names = ['MSFT', 'AAPL', 'GOOG'],
-        // create the chart when all data is loaded
-        createChart = function () {
+$(function() {
+    var chart = new Highcharts.StockChart({
 
-            $('#container').highcharts('StockChart', {
+        chart: {
+            renderTo: 'container'
+        },
 
-                rangeSelector: {
-                    selected: 4
-                },
-
-                rangeSelector: {
-                 selected: 4,
-                 inputEnabled: false,
-                 buttonTheme: {
-                     visibility: 'hidden'
+        rangeSelector: {
+            selected: 1
+        },
+        plotOptions:{
+             series:{
+                 allowPointSelect: true,
+                 marker: {
+                     states: {
+                         select: {
+                             fillColor: 'black',
+                             lineWidth: 0
+                         }
+                     }
                  },
-                 labelStyle: {
-                     visibility: 'hidden'
+                 point: {
+                     events:{
+                         click: function () {
+                           document.getElementById("dat-data").innerHTML = 'Category: ' + this.category + ', value: ' + this.y;
+                           alert(this.y + '+' + (this.x +1) + this.name );
+                         }
+                     }
                  }
-                },
+             }
+         },
+        series: [{
+            name: 'USD to EUR',
+            id: 'dataseries',
+            data: usdeur,
+            events: {
+                click: function(event) {
 
-                yAxis: {
-                    labels: {
-                        formatter: function () {
-                            return (this.value > 0 ? ' + ' : '') + this.value + '%';
-                        }
-                    },
-                    plotLines: [{
-                        value: 0,
-                        width: 2,
-                        color: 'silver'
-                    }]
-                },
+                    this.chart.series[1].addPoint({
+                        x: event.point.x,
+                        title: ''
+                    });
+                }
 
-                plotOptions:{
-                    series:{
-                        allowPointSelect: true,
-                        marker: {
-                            states: {
-                                select: {
-                                    fillColor: 'black',
-                                    lineWidth: 0
-                                }
-                            }
-                        },
-                        point: {
-                            events:{
-                                click: function () {
-                                  document.getElementById("dat-data").innerHTML = 'Category: ' + this.category + ', value: ' + this.y;
-                                  alert(this.y + '+' + (this.x +1) + this.name );
-                                }
-                            }
-                        }
-                    }
-                },
-
-                tooltip: {
-                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                    valueDecimals: 2
-                },
-
-                series: seriesOptions
-            });
-        };
-
-    $.each(names, function (i, name) {
-
-        $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
-
-            seriesOptions[i] = {
-                name: name,
-                data: data,
-                visible: false
-              }
-
-            // As we're loading the data asynchronously, we don't know what order it will arrive. So
-            // we keep a counter and create the chart when all the data is loaded.
-            seriesCounter += 1;
-
-            if (seriesCounter === names.length) {
-                createChart();
-            }
-        });
+            }},
+        {
+            type: 'flags',
+            data: [],
+            onSeries: 'dataseries',
+            shape: 'circlepin',
+            shape: 'url(kitty.png)',
+            width: 12,
+            }]
     });
-
-    $('#clearit').click(function() {
-      var chart = $('#container').highcharts();
-      var series = chart.series;
-       for (i = 0; i < series.length; i++){
-         series[i].hide();
-       }
-     });
-
-    // plot buttons
-    $('#aapl').click(function() {
-       var chart = $('#container').highcharts();
-       var series = chart.series;
-       var seriesIndex = 1
-       if(series[seriesIndex].visible) {
-           series[seriesIndex].hide();
-       } else {
-           series[seriesIndex].show();
-       }
-   });
-
-   $('#msft').click(function() {
-      var chart = $('#container').highcharts();
-      var series = chart.series;
-      var seriesIndex = 0
-      if(series[seriesIndex].visible) {
-          series[seriesIndex].hide();
-      } else {
-          series[seriesIndex].show();
-      }
-  });
-
-  $('#goog').click(function() {
-     var chart = $('#container').highcharts();
-     var series = chart.series;
-     var seriesIndex = 2
-     if(series[seriesIndex].visible) {
-         series[seriesIndex].hide();
-     } else {
-         series[seriesIndex].show();
-     }
-   });
-
- $('#googandmsft').click(function() {
-    var chart = $('#container').highcharts();
-    var series = chart.series;
-    var seriesIndex = [2,0]
-
-    //Show just the group stuff
-    for (i = 0; i < seriesIndex.length; i++) {
-
-       if(series[seriesIndex[i]].visible) {
-           series[seriesIndex[i]].hide();
-       } else {
-           series[seriesIndex[i]].show();
-       }
-     }
-
-  });
-
 });
