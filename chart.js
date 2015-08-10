8af8a4064be4b737f14f1aae60647d85b65f2e1e
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-08-10 13:43:37 -0700

    adding check boxes to toggle on and off the newly parsed graphs

--- chart.js
+++ chart.js
@@ -38,6 +38,7 @@ $(function() {
         series: [{
             name: 'Roll',
             id: 'dataseries',
+            visible: false,
             data: roll,
             events: {
                 click: function(event) {
@@ -60,7 +61,88 @@ $(function() {
             shape: 'circlepin',
             shape: 'url(pointer.png)',
             width: 12,
-            }]
+          },
+          {
+              name: 'Desiredroll',
+              id: 'dataseries',
+              visible: false,
+              data: desiredroll,
+              events: {
+                  click: function(event) {
+
+                      this.chart.series[1].addPoint({
+                          x: event.point.x,
+                          title: ''
+                      });
+                  }
+
+              }},
+          {
+              type: 'flags',
+              data: [],
+              title: ' #',
+              style: { // text style
+                      color: 'white'
+                  },
+              onSeries: 'dataseries',
+              shape: 'circlepin',
+              shape: 'url(pointer.png)',
+              width: 12,
+            },
+            {
+                name: 'Pitch',
+                id: 'dataseries',
+                visible: false,
+                data: pitch,
+                events: {
+                    click: function(event) {
+
+                        this.chart.series[1].addPoint({
+                            x: event.point.x,
+                            title: ''
+                        });
+                    }
+
+                }},
+            {
+                type: 'flags',
+                data: [],
+                title: ' #',
+                style: { // text style
+                        color: 'white'
+                    },
+                onSeries: 'dataseries',
+                shape: 'circlepin',
+                shape: 'url(pointer.png)',
+                width: 12,
+              },
+              {
+                  name: 'Desiredpitch',
+                  id: 'dataseries',
+                  visible: false,
+                  data: desiredpitch,
+                  events: {
+                      click: function(event) {
+
+                          this.chart.series[1].addPoint({
+                              x: event.point.x,
+                              title: ''
+                          });
+                      }
+
+                  }},
+              {
+                  type: 'flags',
+                  data: [],
+                  title: ' #',
+                  style: { // text style
+                          color: 'white'
+                      },
+                  onSeries: 'dataseries',
+                  shape: 'circlepin',
+                  shape: 'url(pointer.png)',
+                  width: 12,
+                  }]
     });
     $('#clearit').click(function() {
       var chart = $('#container').highcharts();
@@ -69,4 +151,44 @@ $(function() {
          series[i].hide();
        }
      });
+    $('#roll').click(function() {
+      var chart = $('#container').highcharts();
+      var series = chart.series;
+      var seriesIndex = 0
+      if(series[seriesIndex].visible) {
+        series[seriesIndex].hide();
+      } else {
+        series[seriesIndex].show();
+      }
+    });
+    $('#desiredroll').click(function() {
+      var chart = $('#container').highcharts();
+      var series = chart.series;
+      var seriesIndex = 2
+      if(series[seriesIndex].visible) {
+        series[seriesIndex].hide();
+      } else {
+        series[seriesIndex].show();
+      }
+    });
+    $('#pitch').click(function() {
+      var chart = $('#container').highcharts();
+      var series = chart.series;
+      var seriesIndex = 4
+      if(series[seriesIndex].visible) {
+        series[seriesIndex].hide();
+      } else {
+        series[seriesIndex].show();
+      }
+    });
+    $('#desiredpitch').click(function() {
+      var chart = $('#container').highcharts();
+      var series = chart.series;
+      var seriesIndex = 2
+      if(series[seriesIndex].visible) {
+        series[seriesIndex].hide();
+      } else {
+        series[seriesIndex].show();
+      }
+    });
 });
